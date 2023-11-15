9542f1cb68ffe57fdb188a8a7074ece9faa31c14
Angelo David Díaz López <84221235+30225@users.noreply.github.com>
2023-11-15 10:32:22 -0500

    Update +page.svelte

--- src/routes/payments/+page.svelte
+++ src/routes/payments/+page.svelte
@@ -2,7 +2,7 @@
 <script>
 	import { onMount } from 'svelte';
 	import AppBar from '../appbar.svelte';
-	const url = 'http://localhost:8000/payments';
+	const url = 'https://angel-fenix.onrender.com/payments';
 	let data = [];
 
 	const getproducts = async () => {
