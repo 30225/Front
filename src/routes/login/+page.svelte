a42596188f651898b938236dbe23b8a0b9650415
Angelo David Díaz López <84221235+30225@users.noreply.github.com>
2023-11-27 06:05:23 +0000

    update

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,98 @@
 <script>
-    import Appbar from "../appbar.svelte";
+    import { onMount } from 'svelte';
+    import Appbar from '../appbar.svelte';
+  
+    let username = '';
+    let password = '';
+  
+    const registerUser = async () => {
+      // Your registration logic here
+    };
+  
+    let loginUsername = '';
+    let loginPassword = '';
+  
+    const loginUser = async () => {
+      const response = await fetch('https://angel-fenix.onrender.com/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: loginUsername, password: loginPassword }),
+      });
+  
+      if (response.ok) {
+        const userData = await response.json();
+        console.log('Login successful!', userData);
+        // Handle successful login (e.g., redirect, update state, etc.)
+      } else {
+        console.error('Login failed.');
+        // Handle login failure (e.g., show error message)
+      }
+    };
+  
+    onMount(() => {
+      // Your initialization code here
+    });
+  </script>
 
-</script>
-
-<Appbar/>
-
+  <Appbar/>
+  
+  <main>
+    <h1>Login</h1>
+  
+    <form on:submit|preventDefault={loginUser}>
+      <label for="login-username">Username:</label>
+      <input type="text" id="login-username" bind:value={loginUsername} required />
+  
+      <label for="login-password">Password:</label>
+      <input type="password" id="login-password" bind:value={loginPassword} required />
+  
+      <button type="submit">Login</button>
+    </form>
+  </main>
+  
+  <style>
+    main {
+      max-width: 400px;
+      margin: 2rem auto 0;
+      padding: 2rem;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+  
+    h1 {
+      text-align: center;
+      margin-bottom: 1rem;
+    }
+  
+    form {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+    }
+  
+    label {
+      font-weight: bold;
+    }
+  
+    input {
+      padding: 0.5rem;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+    }
+  
+    button {
+      padding: 0.75rem;
+      background-color: #007bff;
+      color: #fff;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+  
+    button:hover {
+      background-color: #0056b3;
+    }
+  </style>
