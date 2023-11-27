32fb2ac657f510b59239ac9203856ba7a3066d38
Angelo David Díaz López <84221235+30225@users.noreply.github.com>
2023-11-27 07:04:58 +0000

    update

---
+++ src/routes/cart/+page.svelte
@@ -0,0 +1,63 @@
+<!-- Cart.svelte -->
+
+<script>
+  import { onMount } from 'svelte';
+  import { stores } from '@sveltejs/kit';
+  const { user } = stores();
+
+  let cartItems = [];
+
+  onMount(async () => {
+    // Fetch data from the /cart endpoint
+    const response = await fetch('/cart', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: $user.username }), // Assuming user store contains the username
+    });
+
+    if (response.ok) {
+      cartItems = await response.json();
+    } else {
+      console.error('Failed to fetch cart data');
+    }
+  });
+</script>
+
+<main>
+  <h1>Your Cart</h1>
+
+  {#if cartItems.length > 0}
+    <ul>
+      {#each cartItems as item (item.id)}
+        <li>{item.name} - {item.price}</li>
+      {/each}
+    </ul>
+  {:else}
+    <p>Your cart is empty.</p>
+  {/if}
+</main>
+
+<style>
+  /* Add your cart page styling here */
+  main {
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+
+  h1 {
+    font-size: 24px;
+    margin-bottom: 20px;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  li {
+    margin-bottom: 10px;
+  }
+</style>
